2f71171963657b293cfa2fa801418d0a1fd1bdd6
kingvikram25 <sikarwarvikram88@gmail.com>
2022-07-20 15:51:42 +0530

    js

---
+++ Up-going/1-Variable/index.js
@@ -0,0 +1,11 @@
+let data = prompt("Enter Your Name");
+      alert(`Good Morning, ${data}`);
+
+      // <!-- ## Basic Operation, Mathematical Operation  -->
+      let amount = 2080;
+      let value = amount - 80;
+      let plusVal = amount + 200;
+      let mulVal = amount * 4;
+      let divVal = amount/21;
+
+      // <!-- Logical Operation -->
